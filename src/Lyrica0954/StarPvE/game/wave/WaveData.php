e5948927b39764b537bec33b118a10964ee0306d
SkyLake-git <70795425+SkyLake-git@users.noreply.github.com>
2022-02-05 17:23:22 +0900

    WaveData - CustomWaveStartを引数に追加

--- src/Lyrica0954/StarPvE/game/wave/WaveData.php
+++ src/Lyrica0954/StarPvE/game/wave/WaveData.php
@@ -11,13 +11,16 @@ class WaveData {
 
     protected string $titleFormat;
 
+    protected ?CustomWaveStart $customWaveStart;
+
     public WaveMonsters $lane1;
     public WaveMonsters $lane2;
     public WaveMonsters $lane3;
     public WaveMonsters $lane4;
 
-    public function __construct(string $titleFormat, WaveMonsters $lane1, WaveMonsters $lane2, WaveMonsters $lane3, WaveMonsters $lane4){
+    public function __construct(string $titleFormat, ?CustomWaveStart $customWaveStart, WaveMonsters $lane1, WaveMonsters $lane2, WaveMonsters $lane3, WaveMonsters $lane4){
         $this->titleFormat = $titleFormat;
+        $this->customWaveStart = $customWaveStart;
         
         $this->lane1 = $lane1;
         $this->lane2 = $lane2;
@@ -27,5 +30,9 @@ class WaveData {
 
     public function parseTitleFormat(int $wave){
         return sprintf($this->titleFormat, $wave);
+    }
+
+    public function getCustomWaveStart(): ?CustomWaveStart{
+        return $this->customWaveStart;
     }
 }
